84cadea8941a9ab64aa493c9361563144ed9bedb
Gavin Mogan <git@gavinmogan.com>
2022-04-03 15:13:34 -0700

    Netlify cms is working

--- src/cms/netlify.jsx
+++ src/cms/netlify.jsx
@@ -1,41 +1,40 @@
 import CMS from 'netlify-cms-app';
 import React from 'react';
+import Layout from '../layout';
 
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
+import remark from 'remark';
+import remarkHtml from 'remark-html'
+
 import UserStory from '../components/UserStory';
 
+const UserStoryPreview = ({entry, widgetsFor, getAsset}) => {
+    const data = entry.toJS().data;
+    const paragraphs = widgetsFor('body').getIn(['data', 'paragraphs'])
+    console.log('data', paragraphs.get(0));
+    for (let i = 0; i < data.body.paragraphs.length; i++) {
+        data.body.paragraphs[i] = {
+            html: remark().use(remarkHtml).processSync(paragraphs.get(i)).toString(),
+        }
+    }
 
-const UserStoryPreview = ({entry, widgetFor, getAsset}) => {
     const props = {
-        frontmatter: {
-            title: entry.getIn(['data', 'title']),
-            submitted_by: entry.getIn(['data', 'submitted_by']),
-            date: entry.getIn(['data', 'date']),
-            tag_line: entry.getIn(['data', 'tag_line']),
-            image: getAsset(entry.getIn(['data', 'image'])).url,
-            organization: entry.getIn(['data', 'organization']),
-            industries: entry.getIn(['data', 'industries']),
-            programming_languages: entry.getIn(['data', 'programming_languages']),
-            platforms: entry.getIn(['data', 'platforms']),
-            version_control_systems: entry.getIn(['data', 'version_control_systems']),
-            community_supports: entry.getIn(['data', 'community_supports']),
-            quotes: entry.getIn(['data', 'quotes']),
-            name: entry.getIn(['data', 'name']),
-            location: entry.getIn(['data', 'location']),
-            latitude: entry.getIn(['data', 'latitude']),
-            longitude: entry.getIn(['data', 'longitude']),
-        },
+        ...data,
+        image: getAsset(entry.getIn(['data', 'image'])).url,
     };
     console.log('props', props);
-    console.log('image', getAsset('image'));
-    return (<UserStory {...props}>{widgetFor('body')}</UserStory>);
+    return (
+        <Layout title={"foo"}>
+            <UserStory {...props} />
+        </Layout>
+    );
 };
 
 UserStoryPreview.propTypes = {
     entry: ImmutablePropTypes.map.isRequired,
-    widgetFor: PropTypes.func.isRequired,
+    widgetsFor: PropTypes.func.isRequired,
     getAsset: PropTypes.func.isRequired,
 };
 
