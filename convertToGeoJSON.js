cc09b0a2442a591a36fa1266ff8129dfc0a75a7f
Birajit Saikia <birajitsaikia@gmail.com>
2024-12-14 18:28:50 +0530

    corrected the errors of ci check by updating the convertToGeoJSON.js file

--- convertToGeoJSON.js
+++ convertToGeoJSON.js
@@ -5,41 +5,36 @@ const yaml = require('js-yaml');
 const folderPath = path.join(__dirname, 'src/user-story');
 
 const convertToGeoJSON = (latitude, longitude) => {
-  return {
-    type: "Point",
-    coordinates: [parseFloat(longitude), parseFloat(latitude)]
-  };
+    return {
+        type: 'Point',
+        coordinates: [parseFloat(longitude), parseFloat(latitude)]
+    };
 };
 
 function readFilesRecursively(dir) {
-  const files = fs.readdirSync(dir);
-  
-  files.forEach(file => {
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
+    const files = fs.readdirSync(dir);
+    
+    files.forEach(file => {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
 
-    if (stat.isDirectory()) {
-      // Recursively read subfolders
-      readFilesRecursively(filePath);
-    } else if (file.endsWith('index.yaml')) {
-      const fileContents = fs.readFileSync(filePath, 'utf8');
-      const data = yaml.load(fileContents);
-      
-      if (data.map && data.map.latitude && data.map.longitude) {
-        const latitude = data.map.latitude;
-        const longitude = data.map.longitude;
-        
-        const geoJSON = convertToGeoJSON(latitude, longitude);
-        
-        console.log(`GeoJSON for ${file}:`, JSON.stringify(geoJSON, null, 2));
-        
-        const geoJSONFilePath = path.join(dir, `${path.basename(file, '.yaml')}_geojson.json`);
-        fs.writeFileSync(geoJSONFilePath, JSON.stringify(geoJSON, null, 2));
-      } else {
-        console.log(`Skipping ${file}: Latitude or Longitude not found`);
-      }
-    }
-  });
+        if (stat.isDirectory()) {
+            readFilesRecursively(filePath);
+        } else if (file.endsWith('index.yaml')) {
+            const fileContents = fs.readFileSync(filePath, 'utf8');
+            const data = yaml.load(fileContents);
+            
+            if (data.map && data.map.latitude && data.map.longitude) {
+                const latitude = data.map.latitude;
+                const longitude = data.map.longitude;
+                
+                const geoJSON = convertToGeoJSON(latitude, longitude);
+                
+                const geoJSONFilePath = path.join(dir, `${path.basename(file, '.yaml')}_geojson.json`);
+                fs.writeFileSync(geoJSONFilePath, JSON.stringify(geoJSON, null, 2));
+            }
+        }
+    });
 }
 
 readFilesRecursively(folderPath);
