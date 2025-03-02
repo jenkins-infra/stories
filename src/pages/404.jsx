4e43126aebe3d05b4d79f7bb469887bba2e6f433
TenzDelek <tibetdelek@gmail.com>
2025-03-02 12:06:41 +0530

    typo

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
-import './NotFound.css';
+import './notFound.css';
 
 const NotFoundPage = () => {
   return (
