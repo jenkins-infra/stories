cfbe2f78202caa547b2ff13b2a2dc638986aeeeb
TenzDelek <tibetdelek@gmail.com>
2025-03-03 11:20:15 +0530

    navbar added

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,37 +1,42 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
+import Layout from '../layout';
 import './notFound.css';
 
 const NotFoundPage = () => {
+  const title = 'Page not found';
+  
   return (
-    <main className="page">
-      <title>Not found</title>
-      <StaticImage
-        src="../images/fire-jenkins.svg"
-        alt="Jenkins on fire logo"
-        className="image"
-      />
-      <h1 className="heading">Page not found</h1>
-      <p className="paragraph">
-        Sorry{' '}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{' '}
+    <Layout title={title}>
+      <main className="page">
+        <title>{title}</title>
+        <StaticImage
+          src="../images/fire-jenkins.svg"
+          alt="Jenkins on fire logo"
+          className="image"
+        />
+        <h1 className="heading">Page not found</h1>
+        <p className="paragraph">
+          Sorry{' '}
+          <span role="img" aria-label="Pensive emoji">
+            😔
+          </span>{' '}
         we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
+          <br />
+          {process.env.NODE_ENV === 'development' ? (
+            <>
+              <br />
             Try creating a page in <code className="code">src/pages/</code>
             .
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/" className="link">Go home</Link>.
-      </p>
-    </main>
+              <br />
+            </>
+          ) : null}
+          <br />
+          <Link to="/" className="link">Go home</Link>.
+        </p>
+      </main>
+    </Layout>
   );
 };
 
