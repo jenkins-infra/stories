f4584ba245f4dab7d1414a65abd02672dd3572ab
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-03-27 10:02:45 +0530

    Fix user story navigation links order

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ const path = require('path');
 async function createUserStoryPages({graphql, createPage, createRedirect}) {
     const userStory = path.resolve('src/pages/_user_story.jsx');
     const result = await graphql(`{
-      stories: allUserStory {
+      stories: allUserStory (sort: { fields: [date], order: DESC }) {
         edges {
           node {
             id
