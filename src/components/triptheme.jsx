c430a1fa1df9450a04de74c44401db52e00efa77
Himanshu A Garode <himanshu2006garode@gmail.com>
2025-04-10 18:31:35 +0530

    Formatting Done!

--- src/components/triptheme.jsx
+++ src/components/triptheme.jsx
@@ -29,11 +29,29 @@ const TripTheme = () => {
           <line x1="12" y1="1" x2="12" y2="4" stroke="orange"></line>
           <line x1="12" y1="20" x2="12" y2="23" stroke="orange"></line>
           <line x1="4.22" y1="4.22" x2="6.34" y2="6.34" stroke="orange"></line>
-          <line x1="17.66" y1="17.66" x2="19.78" y2="19.78" stroke="orange"></line>
+          <line
+            x1="17.66"
+            y1="17.66"
+            x2="19.78"
+            y2="19.78"
+            stroke="orange"
+          ></line>
           <line x1="1" y1="12" x2="4" y2="12" stroke="orange"></line>
           <line x1="20" y1="12" x2="23" y2="12" stroke="orange"></line>
-          <line x1="4.22" y1="19.78" x2="6.34" y2="17.66" stroke="orange"></line>
-          <line x1="17.66" y1="6.34" x2="19.78" y2="4.22" stroke="orange"></line>
+          <line
+            x1="4.22"
+            y1="19.78"
+            x2="6.34"
+            y2="17.66"
+            stroke="orange"
+          ></line>
+          <line
+            x1="17.66"
+            y1="6.34"
+            x2="19.78"
+            y2="4.22"
+            stroke="orange"
+          ></line>
         </svg>
       ) : (
         <svg
