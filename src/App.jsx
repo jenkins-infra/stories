48e87f2992d68f899303856c15e503b65797f46a
Vatsal-Verma <lastav1234@gmail.com>
2026-05-13 15:10:41 +0530

    feat: setup new project architecture

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import '@jenkinsci/jenkins-io-components'
+function App() {
+  return(<>
+    <jio-navbar></jio-navbar>
+    <jio-footer></jio-footer>
+  </>)
+}
+
+export default App
