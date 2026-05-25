66d61fd51779806ce090c5bf6cb9874966ba9dd4
Vatsal-Verma <lastav1234@gmail.com>
2026-05-25 21:18:50 +0530

    added vite-plugin-image-optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    ViteImageOptimizer({
+      png: {
+        quality: 80
+      },
+
+      jpeg: {
+        quality: 80
+      },
+
+      jpg: {
+        quality: 80
+      },
+
+      webp: {
+        quality: 80
+      },
+
+      avif: {
+        quality: 70
+      },
+
+      svg: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false
+          }
+        ]
+      }
+    })
+  ]
 })
