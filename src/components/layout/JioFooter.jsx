c62c9529e7cc194a412538565fc4e1cd2c7b2451
Vatsal-Verma <lastav1234@gmail.com>
2026-06-23 14:25:32 +0530

    GSoC 2026: add improve and report actions in jio-footer

--- src/components/layout/JioFooter.jsx
+++ src/components/layout/JioFooter.jsx
@@ -1,5 +1,13 @@
-function JioFooter({ property }) {
-  return <jio-footer property={property}></jio-footer>;
+function JioFooter({ property, sourcePath, githubRepo, githubBranch, reportAProblemTemplate }) {
+  return (
+    <jio-footer
+      githubRepo={sourcePath ? githubRepo : ''}
+      property={property}
+      sourcePath={sourcePath}
+      githubBranch={githubBranch}
+      reportAProblemTemplate={reportAProblemTemplate}
+    ></jio-footer>
+  );
 }
 
 export default JioFooter;
